76a6d4cfd7cdccb05d0d6876a1f25dbbefecc16d
Joe Edelman <joe@citizenlogistics.com>
2010-10-24 17:43:42 -0700

    Version bump to 0.0.1

--- lib/simplvisor/bluepill.rb
+++ lib/simplvisor/bluepill.rb
@@ -1,27 +1,33 @@
 require 'bluepill'
 FileUtils.mkdir_p 'log'
 
-Simplvisor::WATCHES = []
+module Simplvisor
+  WATCHES = []
+  PWD     = ENV['PWD']
+
+  def self.run_bluepill
+    Bluepill.application(ARGV[0] || 'app',
+      :log_file => "#{PWD}/log/bluepill.log",
+      :base_dir => '/tmp',
+      :foreground => true
+    ) do |app|
+      WATCHES.each do |cmdline, options|
+        name = options[:name] || File.basename(cmdline.split.first)
+        app.process(name) do |x|
+          x.working_dir   = PWD
+          x.start_command = cmdline
+          x.pid_file      = options[:pid] || "#{PWD}/log/#{name}.pid"
+          x.daemonize     = true unless options[:reckless]
+          x.stdout        = "#{PWD}/log/#{name}-out.log"
+          x.stderr        = "#{PWD}/log/#{name}-err.log"
+        end
+      end
+    end
+  end
+end
 
 def watch cmdline, options = {}
   Simplvisor::WATCHES << [cmdline, options]
 end
 
-at_exit do
-  Bluepill.application(ARGV[0] || 'app',
-    :log_file => 'log/bluepill.log',
-    :base_dir => '.',
-    :foreground => true
-  ) do |app|
-    Simplvisor::WATCHES.each do |cmdline, options|
-      name = options[:name] || cmdline.split.first
-      app.process(name) do |x|
-        x.start_command = cmdline
-        x.pid_file      = options[:pid] || "log/#{name}.pid"
-        x.daemonize     = true unless options[:reckless]
-        x.stdout        = "log/#{name}-out.log"
-        x.stderr        = "log/#{name}-err.log"
-      end
-    end
-  end
-end
+at_exit{ Simplvisor.run_bluepill }
